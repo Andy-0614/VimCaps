b339237d6c7c1f0a6d8727fc92f33d3e1853e72e
Andy-0614 <andytest.0908@gmail.com>
2026-08-12 18:09:47 +0800

    feat: 紀錄被按下的 KeyCode 跟 有沒有組合按鍵

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use std::collections::BTreeSet;
+use std::sync::Mutex;
+
+use windows::Win32::Foundation::{HMODULE, LPARAM, LRESULT, WPARAM};
+use windows::Win32::System::LibraryLoader::GetModuleHandleW;
+use windows::Win32::UI::WindowsAndMessaging::{
+    CallNextHookEx, DispatchMessageW, GetMessageW, KBDLLHOOKSTRUCT, MSG, SetWindowsHookExW,
+    TranslateMessage, WH_KEYBOARD_LL, WM_KEYDOWN, WM_KEYUP, WM_SYSKEYDOWN, WM_SYSKEYUP,
+};
+
+static HELD: Mutex<BTreeSet<u32>> = Mutex::new(BTreeSet::new());
+
+unsafe extern "system" fn hook_proc(code: i32, wparam: WPARAM, lparam: LPARAM) -> LRESULT {
+    if code >= 0 {
+        let kb = unsafe { &*(lparam.0 as *const KBDLLHOOKSTRUCT) };
+
+        let mut held = HELD.lock().unwrap();
+
+        match wparam.0 as u32 {
+            WM_KEYDOWN | WM_SYSKEYDOWN => {
+                if held.insert(kb.vkCode) {
+                    let line = held
+                        .iter()
+                        .map(u32::to_string)
+                        .collect::<Vec<_>>()
+                        .join(" + ");
+                    println!("{line}");
+                }
+            }
+            WM_KEYUP | WM_SYSKEYUP => {
+                held.remove(&kb.vkCode);
+            }
+            _ => {}
+        }
+    }
+
+    unsafe { CallNextHookEx(None, code, wparam, lparam) }
+}
+
 fn main() {
-    println!("Hello World!");
+    unsafe {
+        let hinstance: HMODULE = GetModuleHandleW(None).expect("GetModuleHandleW failed");
+
+        SetWindowsHookExW(WH_KEYBOARD_LL, Some(hook_proc), hinstance, 0)
+            .expect("SetWindowsHookExW failed");
+
+        let mut msg = MSG::default();
+        while GetMessageW(&mut msg, None, 0, 0).into() {
+            let _ = TranslateMessage(&msg);
+            DispatchMessageW(&msg);
+        }
+    }
 }
